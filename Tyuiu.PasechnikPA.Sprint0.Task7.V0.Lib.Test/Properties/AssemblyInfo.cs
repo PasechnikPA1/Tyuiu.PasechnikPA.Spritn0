71455678d1733fa6c7a24ebe2e250677bbfdf45d
Pavel Pasechnik <Tyuiu.PasechnikPA@outlook.com>
2023-09-15 12:08:58 +0500

    Task7

---
+++ Tyuiu.PasechnikPA.Sprint0.Task7.V0.Lib.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PasechnikPA.Sprint0.Task7.V0.Lib.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PasechnikPA.Sprint0.Task7.V0.Lib.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07447658-b812-4a38-b7d6-35ed7e424bc7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
